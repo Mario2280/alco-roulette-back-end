744dc8637a4dd0efbbbdc486210c70f262fbe8b7
David <david.gulkevich@mail.ru>
2021-08-27 02:05:38 +0300

    asd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send("asdasd"));
 app.post('/auth/login', (req, res) => {
-    res.status(200).send(`aaaaaasdasd ${req.body}`);
+    res.send(`aaaaaasdasd`);
     console.log(req.body);
 });
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
