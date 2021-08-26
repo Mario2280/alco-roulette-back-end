b11e6999f59169a0c4c54663c509d49b2673f9e7
David <david.gulkevich@mail.ru>
2021-08-27 00:17:17 +0300

    second something

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.get('/lexa', async (req, res) => {
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://Mario210:2009747557d1D1@cluster0.jeg3c.mongodb.net/shop', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
+        // await mongoose.connect('mongodb+srv://Mario210:2009747557d1D1@cluster0.jeg3c.mongodb.net/shop', {
+        //     useNewUrlParser: true,
+        //     useFindAndModify: false
+        // })
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
