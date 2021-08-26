9ae2d8c93b1ea25085e41f80cd15b0169f55641c
David <david.gulkevich@mail.ru>
2021-08-27 01:47:15 +0300

    asd

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
 const express = require('express');
-//const mongoose = require('mongoose');
 const app = express();
-const {Router} = require('express');
-const router = Router();
-//const session = require('express-session');
-//const varMiddleware = require('./middleware/variables')
-const PORT =  3000;
-
-// app.use(session({
-//     secret: 'some secret',
-//     resave: false,
-//     saveUninitialized: false,
-// }))
-// app.use(varMiddleware)
-
-
-
-// router.post('/login', async (req, res) => {
-//     req.session.isAuthenticated = true;
-//     console.log(req.body);
-// })
-
-// router.get('/logout', async (req, res) => {
-//     res.session.destroy(()=>{
-//         console.log('Session destroyed');
-//     }); 
-//     res.session.isAuthenticated = false;
-// })
-
-// router.get('/lexa', async (req, res) => {
-//     res.redirect('/');
-// })
-
-router.get('/auth/login', (req, res) => {
-    res.status(200).send("Hello world");
-})
-
-app.use(router)
-
-async function start() {
-    try {
-        // await mongoose.connect('mongodb+srv://Mario210:2009747557d1D1@cluster0.jeg3c.mongodb.net/shop', {
-        //     useNewUrlParser: true,
-        //     useFindAndModify: false
-        // })
-        app.listen(() => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-start()
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send("asdasd"));
+app.post('/auth/login', (req, res) => res.send("AAAAAAAAAAAAsdsadasd"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
