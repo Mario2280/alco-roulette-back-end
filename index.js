70318c99e70c6cf6cc8171020ce48a3f1aaffd7e
David <david.gulkevich@mail.ru>
2021-08-27 22:22:57 +0300

    asd

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const token = 'dsjfv2moi42fm4vim433v2';
 var urlencodedParcer = body_parser.urlencoded({extended: false});
 var JSONparser = body_parser.json();
 app.post('/auth/token', urlencodedParcer, (req, res) => {
-    console.log(req.body);
+    try{
+      console.log(req.body);
     if(req.body == token){
       res.send(true);
     } else{
       res.send(false);
     }
+  } catch(e){throw e}
 });
 app.post('/auth/login', JSONparser, (req, res) => {
   console.log(req.body.login, req.body);  
