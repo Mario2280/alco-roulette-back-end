885b31120a430b4853c871367639d12ed928b33b
David <david.gulkevich@mail.ru>
2021-08-27 22:18:58 +0300

    asd

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var body_parser = require('body-parser');
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const token = 'dsjfv2moi42fm4vim433v2';
-app.use(express.json());
+var JSONparser = body_parser.json();
 app.post('/auth/token', (req, res) => {
     console.log(req.body);
     if(req.body == token){
@@ -12,7 +13,7 @@ app.post('/auth/token', (req, res) => {
       res.send(false);
     }
 });
-app.post('/auth/login', (req, res) => {
+app.post('/auth/login', JSONparser, (req, res) => {
   console.log(req.body.login, req.body);  
   if(req.body.login == 'admin' && req.body.password == 'admin'){
       res.send(token);
