7c3eae4bdd903debd1f2d5a5d95d52ebc91f9874
David <david.gulkevich@mail.ru>
2021-08-27 21:48:31 +0300

    asddf

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const token = 'dsjfv2moi42fm4vim433v2';
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
 app.get('/auth/token', (req, res) => {
     if(req.body == token){
       res.send(true);
